10f47cb753b951fc55df24cf13908b346cb053b9
Akhilkondapally99 <Akhilkondapally99@gmail.com>
2025-07-22 12:05:13 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@ public class Student {
     public static void main(String[] args) {
         int rollno = 1596;
         String sname = "Akhil";
-        String course = "Maven"; 
+        String course = "JAVA"; 
         System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
